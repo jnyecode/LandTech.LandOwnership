62adc27eb46be7bb91be9d0c0f698a96d43c6eb4
ninjanye <ninjanye@gmail.com>
2021-03-31 20:59:12 +0100

    Create solution and project files
    LandTech.LandOwnership - .NET Core 5.0 Console App
    LandTech.LandOwnership.Tests - XUnit Unit Test Project

---
+++ LandTech.LandOwnership/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LandTech.LandOwnership
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
