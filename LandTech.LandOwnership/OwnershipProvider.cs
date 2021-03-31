438ea632c31844a3696c9cdb2228da4c300ced82
ninjanye <ninjanye@gmail.com>
2021-03-31 22:28:00 +0100

    Refactored the OwnershipProvider
    Build a dictionary of company relationships - performance being the main
    driver behind this

--- LandTech.LandOwnership/OwnershipProvider.cs
+++ LandTech.LandOwnership/OwnershipProvider.cs
@@ -5,40 +5,53 @@ namespace LandTech.LandOwnership
 {
     public class OwnershipProvider
     {
-        private readonly IEnumerable<CompanyRelationship> _companyRelationship;
-        private readonly IEnumerable<LandOwnership> _landOwnership;
+        private readonly Dictionary<string, string[]> _companyRelationship;
+        private readonly Dictionary<string, string[]> _landOwnership;
 
         public OwnershipProvider(IEnumerable<CompanyRelationship> companyRelationship, IEnumerable<LandOwnership> landOwnership)
         {
-            _companyRelationship = companyRelationship;
-            _landOwnership = landOwnership;
+            _companyRelationship = BuildCompanyRelationships(companyRelationship);
+            _landOwnership = BuildLandOwnershipByCompany(landOwnership);
         }
-
+        
         public OwnershipResult OwnershipFor(string companyId)
         {
-            var directlyOwned = _landOwnership.Count(lo => lo.CompanyId == companyId);
-            var companiesInChain = GetChildCompanies(companyId);
-            var indirectlyOwned = _landOwnership.Count(lo => companiesInChain.Contains(lo.CompanyId));
+            var directlyOwned = _landOwnership.ContainsKey(companyId) ? _landOwnership[companyId].Length : 0;
+            var companiesInChain = GetChildCompanies(companyId).ToHashSet();
+            var indirectlyOwned = _landOwnership.Where(lo => companiesInChain.Contains(lo.Key)).SelectMany(x => x.Value).Count();
 
             return new OwnershipResult {OwnedDirectly = directlyOwned, OwnedIndirectly = indirectlyOwned };
         }
         
         private IEnumerable<string> GetChildCompanies(string companyId)
         {
-            if (!_companyRelationship.Any(cr => cr.ParentId == companyId))
+            if (!_companyRelationship.ContainsKey(companyId))
             {
                 return Enumerable.Empty<string>();
             }
 
             var result = new List<string>();
-            foreach (var company in _companyRelationship.Where(cr => cr.ParentId == companyId))
+            foreach (var company in _companyRelationship[companyId])
             {
-                result.Add(company.CompanyId);
-                result.AddRange(GetChildCompanies(company.CompanyId));
+                result.Add(company);
+                result.AddRange(GetChildCompanies(company));
             }
 
             return result;
         }        
+        
+        private static Dictionary<string, string[]> BuildLandOwnershipByCompany(IEnumerable<LandOwnership> landOwnership)
+        {
+            return landOwnership.GroupBy(lo => lo.CompanyId, lo => lo.LandId)
+                .ToDictionary(x => x.Key, x => x.ToArray());
+        }
+
+        private static Dictionary<string, string[]> BuildCompanyRelationships(IEnumerable<CompanyRelationship> companyRelationship)
+        {
+            return companyRelationship.Where(cr => cr.ParentId != null)
+                .GroupBy(cr => cr.ParentId, cr => cr.CompanyId)
+                .ToDictionary(x => x.Key, x => x.ToArray());
+        }
     }
 
     public class OwnershipResult
