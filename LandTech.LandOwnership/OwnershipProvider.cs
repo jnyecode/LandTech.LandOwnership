a55fa1763517baeaea3460df6e4077c17068cbac
ninjanye <ninjanye@gmail.com>
2021-03-31 21:21:11 +0100

    Import source data
    Data import is completed using the CsvHelper nuget package

---
+++ LandTech.LandOwnership/OwnershipProvider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace LandTech.LandOwnership
+{
+    public class OwnershipProvider
+    {
+        private readonly IEnumerable<CompanyRelationship> _companyRelationship;
+        private readonly IEnumerable<LandOwnership> _landOwnership;
+
+        public OwnershipProvider(IEnumerable<CompanyRelationship> companyRelationship, IEnumerable<LandOwnership> landOwnership)
+        {
+            _companyRelationship = companyRelationship;
+            _landOwnership = landOwnership;
+        }
+        
+    }
+}
--- LandTech.LandOwnership/Program.cs
+++ LandTech.LandOwnership/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using CsvHelper;
+using CsvHelper.Configuration.Attributes;
 
 namespace LandTech.LandOwnership
 {
@@ -6,7 +12,50 @@ namespace LandTech.LandOwnership
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //TODO
+
+            // import Company Relationship
+            IEnumerable<CompanyRelationship> companyRelationship;
+            using (var reader = new StreamReader("Data/company_relations.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                companyRelationship = csv.GetRecords<CompanyRelationship>().ToList();
+                Console.WriteLine($"Company Relationships: {companyRelationship.Count()}");
+            }
+
+            // import Land Ownership
+            IEnumerable<LandOwnership> landOwnership;
+            using (var reader = new StreamReader("Data/land_ownership.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                landOwnership = csv.GetRecords<LandOwnership>().ToList();
+                Console.WriteLine($"Land Ownerships: {landOwnership.Count()}");
+            }
+            
+            // landownership = lo ctor(customer rel, land ownership)
+            var ownershipProvider = new OwnershipProvider(companyRelationship, landOwnership);
+            // landownership.LandFor(companyId);
+
+            Console.ReadLine();
         }
     }
+
+    public class LandOwnership
+    {
+        [Index(0)]
+        public string LandId { get; init; }
+        [Index(1)]
+        public string CompanyId  { get; init; }
+    }
+
+    public class CompanyRelationship
+    {
+        [Index(0)]
+        public string CompanyId { get; init; }
+        [Index(1)]
+        public string Name { get; init; }
+        [Index(2)]
+        public string ParentId { get; init; }
+    }        
+    
 }
